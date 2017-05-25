c8ba5c80b47ab6e194d717d0f9d23994cecac91d
filipelbc <filipelbc@gmail.com>
2017-05-25 15:44:15 -0300

    More precise email id and direct link to html view

--- src/main/java/com/mockmock/htmlbuilder/MailListHtmlBuilder.java
+++ src/main/java/com/mockmock/htmlbuilder/MailListHtmlBuilder.java
@@ -35,6 +35,7 @@ public class MailListHtmlBuilder implements HtmlBuilder
             output += "      <th>From</th>\n";
             output += "      <th>To</th>\n";
             output += "      <th>Subject</th>\n";
+            output += "      <th>Details</th>\n";
             output += "      <th>Action</th>\n";
             output += "    </thead>\n";
             output += "    <tbody>\n";
@@ -77,7 +78,8 @@ public class MailListHtmlBuilder implements HtmlBuilder
             "<tr>\n" +
             "  <td>" + fromOutput + "</td>\n" +
             "  <td>" + toOutput + "</td>\n" +
-            "  <td><a title=\"" + StringEscapeUtils.escapeHtml(mail.getSubject()) + "\" href=\"/view/" + mail.getId() + "\">" + subjectOutput + "</a></td>\n" +
+            "  <td><a title=\"" + StringEscapeUtils.escapeHtml(mail.getSubject()) + "\" href=\"/view/html/" + mail.getId() + "\">" + subjectOutput + "</a></td>\n" +
+            "  <td><a title=\"View detailed info\" href=\"/view/" + mail.getId() + "\"><em>Details</em></a></td>\n" +
             "  <td><a title=\"Delete this mail\" href=\"/delete/" + mail.getId() + "\"><em>Delete</em></a></td>\n" +
             "</tr>";
     }
